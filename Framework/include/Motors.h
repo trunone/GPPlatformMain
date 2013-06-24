ef3d76888173a937dce8bbb753c969bd93efe106
dale <play888111@hotmail.com.tw>
2013-06-24 16:09:57 +0800

    Motors change .h

--- Framework/include/Motors.h
+++ Framework/include/Motors.h
@@ -15,25 +15,31 @@ namespace Robot
 	class Motors
 	{
 	private:
+	
 		Motors();
 		~Motors();
+	int mNodeId;
+	static const bool DEBUG_PRINT = false;
 
-	    static const bool DEBUG_PRINT = false;
-        int mNodeId;
 
         void* motorHandle0;
         void* motorHandle1;
         void* motorHandle2;
 		
-		int SetEnable(void*);
-		int SetDisable(void*);
+	int SetEnable(void*);
+	int SetDisable(void*);
+	
         int OpenDevice(void*, int);
+	int ActivateProfileVelocityMode(void*);
+	int SetVelocityProfile(void*,int,int );
+	int SetVelocity(void*,long);
+	int HaltVelocityMovement(void*);
 	public:
         bool SetEnableAll();
         bool SetDisableAll();
         bool OpenDeviceAll();
 
-		void Initialize();
+	void Initialize();
 	};
 }
 
