e5e9d3e98a90ebcc75ee6346f270ec7c89c7e104
william <smallbor@hotmail.com>
2013-07-31 10:31:02 +0800

    0731

---
+++ Framework/include/ColorModel.h
@@ -0,0 +1,36 @@
+#ifndef _ColorModel_H_
+#define _ColorModel_H_
+#include "VisionStatus.h"
+#include "SegmentationFunction.h"
+
+using namespace std;
+//----------------------------
+
+namespace Robot
+{
+	class ColorModel : public VisionModule
+	{
+	
+
+	private:
+		
+		static ColorModel* m_UniqueInstance;
+		
+		Colormodel();
+		bool HSV_hsvCheckRange_Blue(float hValue, float sValue, float vValue);
+
+		bool HSV_hsvCheckRange_Red(float hValue, float sValue, float vValue);
+
+		bool HSV_hsvCheckRange_Green(float hValue, float sValue, float vValue);
+		void Erosion(unsigned char *source, unsigned char *target);
+		void Dilation(unsigned char *source, unsigned char *target);
+	public:
+		static ColorModel* GetInstance() { return m_UniqueInstance; }
+		
+		~ColorModel();
+		void Process();
+
+		
+	};
+}
+#endif
